d8e3aa40a0c8317608aaf4b7f5a864f8bb8ae9c2
Juan Rosas <rosas.juan@live.com>
2024-06-25 21:53:36 -0300

    token verificado, obtuve el nombre por consola

---
+++ src/main/java/com/one/ForoHub/infra/security/SecurityFilter.java
@@ -0,0 +1,36 @@
+package com.one.ForoHub.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+   @Autowired
+   private TokenService tokenService;
+
+   @Override
+   protected void doFilterInternal(HttpServletRequest request,
+                                   HttpServletResponse response,
+                                   FilterChain filterChain) throws ServletException, IOException {
+      var token = request.getHeader("Authorization");
+
+      if (token == null || token.isEmpty()) {
+         throw new RuntimeException("Token is invalid");
+      }
+      token = token.replace("Bearer ", "");
+
+      System.out.println(token);
+
+      System.out.println(tokenService.getSubject(token));
+
+      filterChain.doFilter(request, response);
+   }
+}
--- src/main/java/com/one/ForoHub/infra/security/TokenService.java
+++ src/main/java/com/one/ForoHub/infra/security/TokenService.java
@@ -1,8 +1,11 @@
 package com.one.ForoHub.infra.security;
 
 import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import com.one.ForoHub.models.user.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -26,14 +29,33 @@ public class TokenService {
                .withClaim("id", user.getId())
                .withExpiresAt(expiryDate())
                .sign(algorithm); //this is the token
-      } catch (
-            JWTCreationException exception){
+
+      } catch (JWTCreationException exception) {
          throw new RuntimeException(); //just not to leave it empty
       }
    }
 
    public Instant expiryDate() {
       return LocalDateTime.now().plusHours(24).toInstant(ZoneOffset.of("-03:00"));
+   }
+
+   public String getSubject(String token) {
+      DecodedJWT verifier = null;
+      try {
+         Algorithm algorithm = Algorithm.HMAC256(apiSecret);
+         verifier = JWT.require(algorithm)
+               .withIssuer("forohub")
+               .build()
+               .verify(token);
+         verifier.getSubject();
+
+      } catch (JWTVerificationException exception){
+         System.out.println(exception.toString());
+      }
+      if (verifier.getSubject() == null) {
+         throw new RuntimeException("Invalid verifier");
+      }
+      return verifier.getSubject();
    }
 
 }
